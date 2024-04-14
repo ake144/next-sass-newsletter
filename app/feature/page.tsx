a5c2749907f7c904281f7d35d45f5fb5fd93f068
ake144 <tamiratake@gmail.com>
2024-04-14 19:21:00 +0300

    little change

--- app/feature/page.tsx
+++ app/feature/page.tsx
@@ -40,13 +40,13 @@ return(<>
 
     <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</h1>
 
-    <p className="mt-4 text-gray-500">Currently I'm working on it.</p>
-    <p>GO back</p>
-    <Link href="/" >Go Back</Link>
-  </div>
+          <p className="mt-4 text-gray-500">Currently I&apos;m working on it.</p>
+        <p>GO back</p>
+    <Link href="/">Go Back</Link>
+
+          </div>
 </div>
 </>
-)
-}
+)}
 
 export default NotFound;
